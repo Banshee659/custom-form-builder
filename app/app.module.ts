b2d54246e50cc23f2ee3ca5a63bafdb4ff6e6553
Thang659 <Duncan732@hotmail.com>
2019-04-04 09:31:22 +1100

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
+import {FavoriteDirective} from './favorite.directive';
+import {CategoryListPipe} from './category-list.pipe';
+import { MediaItemFormComponent} from './media-item-form.component';
+import { MediaItemService} from './media-item.service';
+import {lookupListToken, lookupLists} from './providers';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent
+  ],
+  providers: [
+    MediaItemService,
+    {
+      provide: lookupListToken, useValue: lookupLists
+    }
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
